bdc5f77342a14109351577ebf513de36ec225a52
Keyner Alberto Estrada Madrid <keynercitoem@gmail.com>
2026-02-07 17:38:21 -0500

    Actualización data export base de datos

--- CRUD-REACTJS-NODEJS-MYSQL/database/techdev_empleados.sql
+++ CRUD-REACTJS-NODEJS-MYSQL/database/techdev_empleados.sql
@@ -31,7 +31,7 @@ CREATE TABLE `empleados` (
   `anios` int NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `empleados` (
 
 LOCK TABLES `empleados` WRITE;
 /*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
-INSERT INTO `empleados` VALUES (1,'Camilo',24,'Colombia','Contador',3,'2026-02-07 21:04:11'),(2,'Alejandra',20,'Colombia','Medica',2,'2026-02-07 21:13:05'),(4,'Natalia',32,'Africa','Cloud Analyst',7,'2026-02-07 21:38:56'),(5,'Jairo',20,'Colombia','Arquitecto',1,'2026-02-07 21:39:21'),(6,'Jose',16,'Colombia','Comerciante',3,'2026-02-07 21:39:56'),(7,'Gabriela',33,'Argentina','Especialista ciberseguridad',10,'2026-02-07 21:40:30'),(8,'Carlos',22,'Colombia','Desarrollador Front',3,'2026-02-07 21:41:15'),(9,'Daniela',27,'Brasil','Desarrollador Python',5,'2026-02-07 21:41:45'),(10,'Federico',19,'Colombia','Soporte TI',1,'2026-02-07 21:42:24'),(11,'Pedro',24,'Colombia','Analista Funcional',4,'2026-02-07 21:42:51');
+INSERT INTO `empleados` VALUES (1,'Camilo',24,'Colombia','Contador',3,'2026-02-07 21:04:11'),(2,'Alejandra',20,'Colombia','Medica',2,'2026-02-07 21:13:05'),(4,'Natalia',32,'Africa','Cloud Analyst',7,'2026-02-07 21:38:56'),(5,'Jairo',20,'Colombia','Arquitecto',1,'2026-02-07 21:39:21'),(6,'Jose',16,'Colombia','Comerciante',3,'2026-02-07 21:39:56'),(7,'Gabriela',33,'Argentina','Especialista ciberseguridad',10,'2026-02-07 21:40:30'),(8,'Carlos',22,'Colombia','Desarrollador Front',3,'2026-02-07 21:41:15'),(9,'Daniela',27,'Brasil','Desarrollador Python',5,'2026-02-07 21:41:45'),(10,'Federico',19,'Colombia','Soporte TI',1,'2026-02-07 21:42:24'),(11,'Pedro',24,'Colombia','Analista Funcional',4,'2026-02-07 21:42:51'),(13,'Jenry',25,'Colombia','Técnico Soporte TI',6,'2026-02-07 22:12:47');
 /*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-07 16:47:25
+-- Dump completed on 2026-02-07 17:36:51
